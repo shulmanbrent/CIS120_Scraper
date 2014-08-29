22139ce9a3c094e06a038e38515527291a31526f
shulmanbrent <shulmanbrent@yahoo.com>
2014-08-28 23:44:54 -0400

    Downloads all Lectures and in class code
    first attempt at the beginning of class.
    could be bugs as the page gets updates as the course goes on

---
+++ CIS120Scraper.py
@@ -0,0 +1,42 @@
+import sys
+import os, urllib, re
+from bs4 import BeautifulSoup
+
+
+global local_path
+
+#opens class web page, reads it, and implements BeautifulSoup
+url = urllib.urlopen("http://www.seas.upenn.edu/~cis120/current/lectures.shtml")
+page = url.read()
+parsed = BeautifulSoup(page)
+
+#finds all links in page
+links = parsed.find_all('a', href=True)
+
+for link in links:
+
+    if "lectures/" in str(link):
+        href = link['href']
+        
+        #gets last expression in link
+        file = re.findall('[^\/]+$', href)
+        file = file[0]
+        
+        if "/lec" in str(href):
+            
+            #local directory for my files
+            #changes necessary for use on other computer
+            base_path = "C:\Users\Brent\Documents\CIS 120"
+            
+            file_name = file.replace(".pdf", "")
+            local_path = os.path.join(base_path, file_name)
+            
+            #create folder for files if not already created
+            if not os.path.exists(local_path): os.makedirs(local_path)
+        
+        head = "http://www.seas.upenn.edu/~cis120/current/"
+            
+        #downloads and saves files to chosen directory
+        f = open(str(os.path.join(local_path, file)), 'wb')
+        f.write(urllib.urlopen(os.path.join(head,href)).read())
+        f.close
